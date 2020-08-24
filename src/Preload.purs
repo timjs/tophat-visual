33910c0bc48bfccbc81a5f9a535518348f6ba301
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2020-08-24 10:50:19 +0200

    add a lot of helper functions to the Preload

--- src/Preload.purs
+++ src/Preload.purs
@@ -2,6 +2,26 @@ module Preload
   ( module Reexport
   -- Booleans
   , Bool
+  -- Strings
+  , inbetween
+  , quote
+  , indent
+  -- Arrays
+  , Slice
+  , slice
+  , uncons
+  , unsnoc
+  , tail
+  , init
+  , take
+  , takeEnd
+  , takeWhile
+  , drop
+  , dropEnd
+  , dropWhile
+  , span
+  , group
+  , groupBy
   -- Functions
   , (<<)
   , (>>)
@@ -14,7 +34,7 @@ module Preload
   , Nat
   , nat
   , unnat
-  -- HashSets
+  -- Hashables
   , (=<)
   , (/<)
   , notMember
@@ -35,7 +55,12 @@ module Preload
   , (/:)
   , foldrInfix
   , (:\)
+  , foldr1
   , gather
+  , words
+  , lines
+  , unwords
+  , unlines
   -- Functors, Applicatives, Alternatives, Monads
   , (<||)
   , (||>)
@@ -45,7 +70,7 @@ module Preload
   , (-<)
   , (-|)
   , (|-)
-  , none
+  , skip
   , pair
   , (<>)
   , (|=)
@@ -54,43 +79,121 @@ module Preload
   , using
   ) where
 
----- Reexports
-import Prelude hiding (mempty, pure, (<<<), (>>>), (<>), ($), (#), (<$>), (<#>), (<@>), (<$), ($>), (<*>), (<*), (*>)) as Reexport
+import Control.Applicative (pure)
 import Control.Apply (applyFirst, applySecond) as Reexport
 import Control.Bind (bind, bindFlipped, discard) as Reexport
+import Control.Semigroupoid (composeFlipped)
+import Data.Array as Array
+import Data.Array.NonEmpty (fromNonEmpty, toUnfoldable)
+import Data.ArrayView as Slice
+import Data.ArrayView.Internal (fromNonEmptyArray)
+import Data.ArrayView.Internal as Slice
 import Data.Bifoldable as Reexport
 import Data.Bifunctor (class Bifunctor, bimap, lmap, rmap) as Reexport
 import Data.Either hiding (Either) as Reexport
 import Data.Either.Nested (type (\/)) as Reexport
 import Data.Enum (class Enum) as Reexport
-import Data.Functor (mapFlipped, voidLeft, voidRight) as Reexport
+import Data.Enum (enumFromTo)
+import Data.Foldable (foldM)
 import Data.Foldable hiding (foldM) as Reexport
 import Data.FoldableWithIndex hiding (foldlDefault, foldrDefault, foldMapDefault) as Reexport
+import Data.Function (apply, applyFlipped)
+import Data.Functor (mapFlipped, voidLeft, voidRight) as Reexport
 import Data.HashMap (HashMap) as Reexport
 import Data.HashSet (HashSet) as Reexport
-import Data.List (List(..)) as Reexport
-import Data.Maybe as Reexport
+import Data.HashSet as HashSet
+import Data.Hashable (class Hashable) as Reexport
+import Data.List (List) as Reexport
+import Data.Maybe hiding (fromMaybe) as Reexport
+import Data.Monoid (mempty)
 import Data.Newtype (class Newtype, wrap, unwrap, ala, over, over2, under, under2) as Reexport
+import Data.NonEmpty (NonEmpty(..)) as Reexport
+import Data.String.CodeUnits as String
+import Data.String.Common as String
+import Data.String.Pattern as String
 import Data.Traversable as Reexport
 import Data.Tuple (curry, fst, lookup, snd, swap, uncurry) as Reexport
 import Data.Tuple.Nested ((/\), type (/\)) as Reexport
--- import Data.Generic.Rep (class Generic) as Reexport
--- import Data.Generic.Rep.Show (genericShow) as Reexport
----- Rest
-import Control.Semigroupoid (composeFlipped)
-import Control.Applicative (pure)
-import Data.Enum (enumFromTo)
-import Data.Monoid (mempty)
-import Data.Foldable (foldM)
-import Data.Function (apply, applyFlipped)
-import Data.Hashable (class Hashable)
-import Data.HashSet (member) as HashSet
-import Unsafe.Coerce (unsafeCoerce)
+import Data.Unfoldable as Reexport
+import Prelude hiding (mempty, pure, (<<<), (>>>), (<>), ($), (#), (<$>), (<#>), (<@>), (<$), ($>), (<*>), (<*), (*>)) as Reexport
 import Prim.TypeError (class Warn, Text)
+import Unsafe.Coerce (unsafeCoerce)
 
 ---- Booleans ------------------------------------------------------------------
 type Bool
   = Boolean
+
+---- Strings -------------------------------------------------------------------
+inbetween :: Char -> Char -> String -> String
+inbetween a b s = String.singleton a ++ s ++ String.singleton b
+
+quote :: String -> String
+quote = inbetween '"' '"'
+
+indent :: Int -> String -> String
+indent n s = Reexport.fold (Reexport.replicate n "  " :: Array String) ++ s
+
+---- Arrays --------------------------------------------------------------------
+type Slice
+  = Slice.ArrayView
+
+-- *O(1)*
+slice :: forall a. Int -> Int -> Array a -> Slice a
+slice n m = Slice.fromArray >> Slice.slice n m
+
+-- *O(1)*
+uncons :: forall a. Array a -> Reexport.Maybe { head :: a, tail :: Slice a }
+uncons = Slice.fromArray >> Slice.uncons
+
+-- *O(1)*
+unsnoc :: forall a. Array a -> Reexport.Maybe { init :: Slice a, last :: a }
+unsnoc = Slice.fromArray >> Slice.unsnoc
+
+-- *O(1)*
+tail :: forall a. Array a -> Reexport.Maybe (Slice a)
+tail = Slice.fromArray >> Slice.tail
+
+-- *O(1)*
+init :: forall a. Array a -> Reexport.Maybe (Slice a)
+init = Slice.fromArray >> Slice.init
+
+-- *O(1)*
+take :: forall a. Int -> Array a -> Slice a
+take n = Slice.fromArray >> Slice.take n
+
+-- *O(1)*
+takeEnd :: forall a. Int -> Array a -> Slice a
+takeEnd n = Slice.fromArray >> Slice.takeEnd n
+
+-- *O(m)*
+takeWhile :: forall a. (a -> Boolean) -> Array a -> Slice a
+takeWhile f = Slice.fromArray >> Slice.takeWhile f
+
+-- *O(1)*
+drop :: forall a. Int -> Array a -> Slice a
+drop n = Slice.fromArray >> Slice.drop n
+
+-- *O(1)*
+dropEnd :: forall a. Int -> Array a -> Slice a
+dropEnd n = Slice.fromArray >> Slice.dropEnd n
+
+-- *O(m)*
+dropWhile :: forall a. (a -> Boolean) -> Array a -> Slice a
+dropWhile f = Slice.fromArray >> Slice.dropWhile f
+
+-- *O(m)*
+span :: forall a. (a -> Boolean) -> Array a -> { init :: Slice a, rest :: Slice a }
+span f = Slice.fromArray >> Slice.span f
+
+group :: forall a. Reexport.Eq a => Array a -> Array (Reexport.NonEmpty Slice a)
+group = Slice.fromArray >> Slice.group >> Reexport.map fromNonEmptyArrayView >> Slice.toArray
+
+groupBy :: forall a. (a -> a -> Boolean) -> Array a -> Array (Reexport.NonEmpty Slice a)
+groupBy f = Slice.fromArray >> Slice.groupBy f >> Reexport.map fromNonEmptyArrayView >> Slice.toArray
+
+-- INTERNAL USE ONLY --
+fromNonEmptyArrayView :: forall a. Slice.NonEmptyArrayView a -> Reexport.NonEmpty Slice a
+fromNonEmptyArrayView (Slice.NonEmptyArrayView xs) = xs
 
 ---- Functions -----------------------------------------------------------------
 infixr 9 Reexport.compose as <<
@@ -128,12 +231,12 @@ instance eqNat :: Reexport.Eq Nat where
 instance ordNat :: Reexport.Ord Nat where
   compare (Nat i) (Nat j) = Reexport.compare i j
 
----- HashSets ------------------------------------------------------------------
+---- Hashables -----------------------------------------------------------------
 infix 4 HashSet.member as =<
 
 infix 4 notMember as /<
 
-notMember :: forall a. Hashable a => a -> Reexport.HashSet a -> Bool
+notMember :: forall a. Reexport.Hashable a => a -> Reexport.HashSet a -> Bool
 notMember x = Reexport.not << HashSet.member x
 
 ---- Semigroups, Monoids, Groups, Modules, Torsors -----------------------------
@@ -177,8 +280,29 @@ foldlInfix x xs f = Reexport.foldl f x xs
 foldrInfix :: forall f a b. Reexport.Foldable f => f a -> b -> (a -> b -> b) -> b
 foldrInfix xs x f = Reexport.foldr f x xs
 
+foldr1 :: forall f a. Reexport.Foldable f => (a -> a -> a) -> f a -> Reexport.Maybe a
+foldr1 f xs = Reexport.foldr mf Reexport.Nothing xs
+  where
+  mf x m =
+    Reexport.Just
+      <| case m of
+          Reexport.Nothing -> x
+          Reexport.Just y -> f x y
+
 gather :: forall f m a b. Reexport.Foldable f => Reexport.Monad m => (a -> b -> m a) -> a -> f b -> m a
 gather = foldM
+
+words :: forall f. Reexport.Unfoldable f => String -> f String
+words = String.split (String.Pattern " ") >> Array.toUnfoldable
+
+lines :: forall f. Reexport.Unfoldable f => String -> f String
+lines = String.split (String.Pattern "\n") >> Array.toUnfoldable
+
+unwords :: forall f. Reexport.Foldable f => f String -> String
+unwords = Reexport.intercalate " "
+
+unlines :: forall f. Reexport.Foldable f => f String -> String
+unlines = Reexport.intercalate "\n"
 
 ---- Functors, Applicatives, Applicatives, Monads ------------------------------
 infixl 4 Reexport.map as <||
@@ -207,8 +331,8 @@ done = pure
 pair :: forall f a b. Reexport.Applicative f => f a -> f b -> f (a Reexport./\ b)
 pair x y = done Reexport.(/\) -< x -< y
 
-none :: forall f. Reexport.Applicative f => f Reexport.Unit
-none = done Reexport.unit
+skip :: forall f. Reexport.Applicative f => f Reexport.Unit
+skip = done Reexport.unit
 
 ---- Newtypes ------------------------------------------------------------------
 using :: forall f s b a t. Reexport.Newtype t a => Reexport.Newtype s b => Reexport.Functor f => (a -> t) -> (f s -> t) -> f b -> a
