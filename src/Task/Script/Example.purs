e7ad0389cbcaa9fad050869eb521b2eae90e1c7e
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2020-10-04 12:08:07 +0200

    fix Examples

--- src/Task/Script/Example.purs
+++ src/Task/Script/Example.purs
@@ -1,35 +1,36 @@
 module Task.Script.Example where
 
 import Preload
-import Task.Script.Syntax (BasicType(..), Constant(..), Expression(..), Match(..), PrimType(..), Statement(..), Task(..), Type(..), Unchecked(..))
+import Task.Script.Syntax (BasicType(..), Constant(..), Expression(..), Match(..), PrimType(..), Task(..), Type(..))
+import Task.Script.Checker (Unchecked(..))
 
 ---- Types ---------------------------------------------------------------------
 t_nationality :: Type
 t_nationality =
   TVariant
     <| from
-        [ "Dutch" : TRecord neutral
-        , "British" : TRecord neutral
-        , "German" : TRecord neutral
+        [ "Dutch" ** TRecord neutral
+        , "British" ** TRecord neutral
+        , "German" ** TRecord neutral
         ]
 
 t_passenger :: Type
 t_passenger =
   TRecord
     <| from
-        [ "first_name" : TPrimitive TString
-        , "last_name" : TPrimitive TString
-        , "nationality" : t_nationality
-        , "age" : TPrimitive TInt
+        [ "first_name" ** TPrimitive TString
+        , "last_name" ** TPrimitive TString
+        , "nationality" ** t_nationality
+        , "age" ** TPrimitive TInt
         ]
 
 t_flight :: Type
 t_flight =
   TVariant
     <| from
-        [ "ToAmsterdam" : TRecord neutral
-        , "ToLondon" : TRecord neutral
-        , "ToBerlin" : TRecord neutral
+        [ "ToAmsterdam" ** TRecord neutral
+        , "ToLondon" ** TRecord neutral
+        , "ToBerlin" ** TRecord neutral
         ]
 
 t_row :: Type
@@ -42,28 +43,27 @@ t_seat :: Type
 t_seat =
   TRecord
     <| from
-        [ "row" : t_row
-        , "chair" : t_chair
+        [ "row" ** t_row
+        , "chair" ** t_chair
         ]
 
 t_booking :: Type
 t_booking =
   TRecord
     <| from
-        [ "passengers" : TList t_passenger
-        , "flight" : t_flight
-        , "seats" : TList t_seat
+        [ "passengers" ** TList t_passenger
+        , "flight" ** t_flight
+        , "seats" ** TList t_seat
         ]
 
 ---- Tasks ---------------------------------------------------------------------
-enter_passenger :: Statement (Unchecked Task)
+enter_passenger :: Task (Unchecked Task)
 enter_passenger =
-  Step (MRecord <| from [ "value" : MBind "passengers" ]) (Unchecked <| Enter (BPrimitive TString) "Passenger details")
-    <| Task
+  Step (MRecord <| from [ "value" ** MBind "passengers" ]) (Unchecked <| Enter (BPrimitive TString) "Passenger details")
     <| Unchecked
     <| Select
     <| from
         [ "Continue"
-            : Constant (B true)
-            : (Task <| Unchecked <| Lift (Record <| from [ "passengers" : Variable "passengers" ]))
+            ** Constant (B true)
+            ** (Unchecked <| Lift (Record <| from [ "passengers" ** Variable "passengers" ]))
         ]
