6a649accbf07516c40486c22637d5bfd4f37b8ad
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2021-09-21 11:31:17 +0200

    add inline and block Dom creators

--- src/Concur/Dom.purs
+++ src/Concur/Dom.purs
@@ -4,6 +4,9 @@ module Concur.Dom
   , Attr
   , Widget
   , Signal
+  -- # Elements
+  , block
+  , inline
   -- # Values
   , stringValue
   , intValue
@@ -16,25 +19,33 @@ import Preload
 import Concur (Widget, Signal) as Concur
 import Concur.React (HTML) as React
 import Concur.React.Props (ReactProps, unsafeTargetValue) as React
+import Concur.Dom.Node as Node
+import Concur.Dom.Attr as Attr
 import Concur.React.Run (runWidgetInDom) as React
 import React.SyntheticEvent (SyntheticEvent_) as React
 import Data.Int as Int
 import Data.Number as Number
 
 ---- Types ---------------------------------------------------------------------
-type Dom
-  = React.HTML
 
-type Attr a
-  = React.ReactProps a
+type Dom = React.HTML
 
-type Widget
-  = Concur.Widget Dom
+type Attr a = React.ReactProps a
 
-type Signal a
-  = Concur.Signal Dom a
+type Widget = Concur.Widget Dom
+
+type Signal a = Concur.Signal Dom a
+
+---- Elements ------------------------------------------------------------------
+
+block :: forall a. Array String -> Array (Widget a) -> Widget a
+block cs = Node.div (map Attr.className cs)
+
+inline :: forall a. Array String -> Array (Widget a) -> Widget a
+inline cs = Node.span (map Attr.className cs)
 
 ---- Target values -------------------------------------------------------------
+
 stringValue :: forall a. React.SyntheticEvent_ a -> String
 stringValue = React.unsafeTargetValue
 
@@ -45,5 +56,6 @@ intValue :: forall a. React.SyntheticEvent_ a -> Maybe Int
 intValue e = floatValue e >-> Int.floor
 
 ---- Running -------------------------------------------------------------------
+
 runWidgetInDom :: forall a. String -> Widget a -> Effect Unit
 runWidgetInDom = React.runWidgetInDom
