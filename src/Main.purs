146b4b99b94bf053e9a3c9555c44067ae93f7a0d
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2021-10-22 21:44:44 +0200

    refactor distribution flow

--- src/Main.purs
+++ src/Main.purs
@@ -1,16 +1,15 @@
 module Main where
 
 import Preload
-import Concur (dynamic)
 import Concur.Dom (runWidgetInDom)
 import Task.Script.Renderer as Renderer
--- import Task.Script.Example.Subsidy as Example
--- import Task.Script.Example.Deadline as Example
-import Task.Script.Example.Singles as Example
+import Task.Script.Example.Subsidy as Example
 
+-- import Task.Script.Example.Deadline as Example
+-- import Task.Script.Example.Singles as Example
 -- import Test.Selection as Test
 -- import Test.Tree as Test
 
 main :: Effect Unit
 -- main = runWidgetInDom "root" <| Test.main
-main = runWidgetInDom "root" <| Renderer.main Example.world "just execute"
+main = runWidgetInDom "root" <| Renderer.main Example.world "request subsidy"
